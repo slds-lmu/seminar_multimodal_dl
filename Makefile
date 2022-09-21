268285911202b0d07164dd91699300fbb9374220
schalkdaniel <d-schalk@t-online.de>
2022-09-21 14:50:32 +0200

    fix many things

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 all: html pdf epub
 
 html: *.Rmd
-	Rscript -e "bookdown::render_book('./', 'bookdown::gitbook')"
+	Rscript -e "bookdown::render_book('./', 'bookdown::gitbook', clean = FALSE)"
 
 pdf: *.Rmd
-	Rscript -e "bookdown::render_book('./', 'bookdown::pdf_book')"
+	Rscript -e "bookdown::render_book('./', 'bookdown::pdf_book', clean = FALSE)"
 
 epub : *.Rmd
-	Rscript -e "bookdown::render_book('./', 'bookdown::epub_book')"
+	Rscript -e "bookdown::render_book('./', 'bookdown::epub_book', clean = FALSE)"
 
--- fix-cits.R
+++ fix-cits.R
@@ -34,3 +34,62 @@ removeEntry = function(cit, bib_file = "book.bib", bib.out = "book.bib.new", ent
 
 invisible(lapply(refs, addEntry, bib_file = "book.bib.new", bib_out = "book.bib.new"))
 
+
+
+### Merge bibfiles:
+
+library(bib2df)
+df <- bib2df("book.bib")
+df = bibtex::read.bib("book.bib")
+
+
+getRefs = function(file = "book.bib") {
+  lines = readLines(file)
+  idx_ref = grep("@", lines)
+  sapply(lines[idx_ref], function(l) {
+    a = strsplit(l, "[{]")[[1]][2]
+    return(strsplit(a, ",")[[1]][1])
+  }, USE.NAMES = FALSE)
+}
+getRefLines = function(ref, file = "book.bib") {
+  lines = readLines(file)
+  lstart = grep(ref, lines, perl = TRUE)
+  lend = lstart + 1
+  rtype = strsplit(lines[lstart], "[{]")[[1]][1]
+  rtype = gsub("@", "", rtype)
+
+  found_end = FALSE
+  while(! found_end) {
+    if (grepl("@", lines[lend]) || (lend == length(lines))) {
+      found_end = TRUE
+    } else {
+      lend = lend + 1
+    }
+  }
+  return(lines[seq(lstart, lend - 1)])
+}
+
+#getRefLines(ref)
+#rfs = getRefs("book.bib")
+
+mergeBibs = function(bprio, badd, bnew) {
+  refs_prio = getRefs(bprio)
+  refs_add = getRefs(badd)
+
+  refs_add = refs_add[! refs_add %in% refs_prio]
+  ls = readLines(bprio)
+  for (ref in refs_add) {
+    message(ref)
+    ls = c(ls, getRefLines(ref, badd))
+  }
+  writeLines(ls, bnew)
+}
+#mergeBibs("book.bib", "bibfiles/03-01img2text_luyang.bib", "tmp.bib")
+
+bfiles = list.files("bibfiles", full.names = TRUE)
+mergeBibs("book.bib", bfiles[1], "tmp.bib")
+for (bf in bfiles[-1]) {
+  message("Reading:", bf)
+  mergeBibs("tmp.bib", bf, "tmp.bib")
+}
+
